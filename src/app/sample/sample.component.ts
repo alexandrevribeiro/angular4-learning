e816bd39b305fe770a5887ffd34d7c84e7991b5d
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-12-12 15:51:47 -0200

    Template options

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -2,7 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  templateUrl: './sample.component.html',
+  // Template options:
+  // Option 1) File URL
+  // templateUrl: './sample.component.html',  
+
+  // Option 2) Inline html
+  // template: '<h5>Sample of inline template.</h5>',
+
+  // Option 3) Multiple lines html
+  template: `
+  <h5>Sample of multiple lines html</h5>
+  <hr/>
+  <p>Person: {{ person.name }}, {{ person.gender }}, {{ person.age }}, {{ person.location }}</p>
+  <hr/>
+
+  <ul>
+    <li *ngFor="let item of jsFrameworks">{{ item }}</li>
+  </ul>
+  <hr/>
+  
+  `,
+
   styleUrls: ['./sample.component.css']
 })
 export class SampleComponent implements OnInit {
@@ -11,5 +31,14 @@ export class SampleComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  person = {
+    name: 'Alexandre',
+    gender: 'male',
+    age: 27,
+    location: 'Brazil'
+  };
+
+  jsFrameworks = ['Vue.js', 'React', 'Angular'];
 
 }
