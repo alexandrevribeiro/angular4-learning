2bfcf8097b1295021854023dc4d4c29736923771
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-12-19 15:49:42 -0200

    Styling - Using ngClass

--- src/app/styling/styling.component.ts
+++ src/app/styling/styling.component.ts
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   <!-- Binding to a property which contains the class name:  -->
   <h1 [class]="titleClass">Using class binding (class name)</h1>
 
-  <!-- Binding to a boolean property determining whether or not the class should be applied   -->
+  <!-- Binding to a boolean property determining whether or not the class should be applied: -->
   <h1 [class.red-title]="isRedClass">Using class binding (boolean property)</h1>
+
+  <!-- Using "ngClass" to bind to an object with multiple classes: -->
+  <h1 [ngClass]="titleClasses">Using class binding (ngClass)</h1>
   `,
   
   styles: [`
@@ -20,10 +23,18 @@ import { Component } from '@angular/core';
     color: #af0000;
   }
 
+  .large-title {
+    font-size: 3em;
+  }
+
   `]
 })
 export class StylingComponent {
 
   titleClass = "red-title";
   isRedClass = true;
+  titleClasses = {
+    'red-title': true,
+    'large-title': true
+  }
 }
